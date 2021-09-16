58e68c4575089974b47bed0ee96d590d9328330d
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2021-09-16 17:36:41 +0900

    toggles

---
+++ src/Lucid/Mdl/Base.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lucid.Mdl.Base
+  ( Ripple (..)
+  , HtmlClass (..)
+  ) where
+
+import Data.Text (Text)
+import GHC.Generics (Generic)
+
+data Ripple = Ripple deriving (Show, Read, Eq, Ord, Generic)
+
+class HtmlClass a where
+  toHtmlClass :: a -> Text
+
+instance HtmlClass a => HtmlClass (Maybe a) where
+  toHtmlClass Nothing = ""
+  toHtmlClass (Just a) = toHtmlClass a
+
+instance HtmlClass Ripple where
+  toHtmlClass _ = "mdl-js-ripple-effect"
---
+++ src/Lucid/Mdl/Toggle.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Toggles. <https://getmdl.io/components/index.html#toggles-section>
+module Lucid.Mdl.Toggle
+  ( checkbox_
+  , radio_
+  , iconToggle_
+  , switch_
+  ) where
+
+import Lucid.Mdl.Base (Ripple, HtmlClass (toHtmlClass))
+
+import qualified Lucid
+import Data.Text (Text)
+
+-- | Checkbox. <https://getmdl.io/components/index.html#toggles-section/checkbox>
+checkbox_ :: Monad m => Lucid.HtmlT m () -> Text -> Maybe Ripple -> [Lucid.Attribute] -> [Lucid.Attribute] -> [Lucid.Attribute] -> Lucid.HtmlT m ()
+checkbox_ label id' ripple checkboxAttributes inputAttributes labelAttributes =
+  Lucid.label_ ([Lucid.classes_ ["mdl-checkbox", "mdl-js-checkbox", toHtmlClass ripple], Lucid.for_ id'] ++ checkboxAttributes) $ do
+    Lucid.input_ ([Lucid.type_ "checkbox", Lucid.id_ id', Lucid.class_ "mdl-checkbox__input"] ++ inputAttributes)
+    Lucid.span_ (Lucid.class_ "mdl-checkbox__label" : labelAttributes) label
+
+-- | Radio button. <https://getmdl.io/components/index.html#toggles-section/radio>
+radio_ :: Monad m => Lucid.HtmlT m () -> Text -> Text -> Maybe Ripple -> [Lucid.Attribute] -> [Lucid.Attribute] -> [Lucid.Attribute] -> Lucid.HtmlT m ()
+radio_ label id' name ripple radioAttributes inputAttributes labelAttributes =
+  Lucid.label_ ([Lucid.classes_ ["mdl-radio", "mdl-js-radio", toHtmlClass ripple], Lucid.for_ id'] ++ radioAttributes) $ do
+    Lucid.input_ ([Lucid.type_ "radio", Lucid.id_ id', Lucid.name_ name, Lucid.class_ "mdl-radio__button"] ++ inputAttributes)
+    Lucid.span_ (Lucid.class_ "mdl-radio__label" : labelAttributes) label
+
+-- | Icon toggle. <https://getmdl.io/components/index.html#toggles-section/icon-toggle>
+iconToggle_ :: Monad m => Lucid.HtmlT m () -> Text -> Maybe Ripple -> [Lucid.Attribute] -> [Lucid.Attribute] -> [Lucid.Attribute] -> Lucid.HtmlT m ()
+iconToggle_ label id' ripple iconToggleAttributes inputAttributes labelAttributes =
+  Lucid.label_ ([Lucid.classes_ ["mdl-icon-toggle", "mdl-js-icon-toggle", toHtmlClass ripple], Lucid.for_ id'] ++ iconToggleAttributes) $ do
+    Lucid.input_ ([Lucid.type_ "checkbox", Lucid.id_ id', Lucid.class_ "mdl-icon-toggle__input"] ++ inputAttributes)
+    Lucid.span_ (Lucid.classes_ ["mdl-icon-toggle__label", "material-icons"] : labelAttributes) label
+
+-- | Switch. <https://getmdl.io/components/index.html#toggles-section/switch>
+switch_ :: Monad m => Text -> Maybe Ripple -> [Lucid.Attribute] -> [Lucid.Attribute] -> [Lucid.Attribute] -> Lucid.HtmlT m ()
+switch_ id' ripple switchAttributes inputAttributes labelAttributes =
+  Lucid.label_ ([Lucid.classes_ ["mdl-switch", "mdl-js-switch", toHtmlClass ripple], Lucid.for_ id'] ++ switchAttributes) $ do
+    Lucid.input_ ([Lucid.type_ "checkbox", Lucid.id_ id', Lucid.class_ "mdl-switch__input"] ++ inputAttributes)
+    Lucid.span_ (Lucid.class_ "mdl-switch__label" : labelAttributes) ""
